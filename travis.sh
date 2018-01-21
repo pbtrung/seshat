85cf2e18023637066c31814d5165180fdf12f56c
Trung Pham <phambaotrung@gmail.com>
2018-01-21 12:02:08 -0800

    Add meson and ninja

--- travis.sh
+++ travis.sh
@@ -3,4 +3,10 @@
 mkdir tmp
 curl -L https://bootstrap.pypa.io/get-pip.py -o tmp/get-pip.py
 python3.6 tmp/get-pip.py --user
-pip -V
+export PATH=$PATH:~/.local/bin
+pip install --user meson
+
+curl -L https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip -o tmp/ninja-linux.zip
+unzip tmp/ninja-linux.zip -d tmp
+export PATH=$PATH:$PWD/tmp
+ninja --version
